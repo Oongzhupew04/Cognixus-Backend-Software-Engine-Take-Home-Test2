32a2a2c0baf7860403933ae1cf952cf5d6c60d13
Oongzhupew04 <vincentoong12345@gmail.com>
2025-05-13 02:11:44 +0800

    Done
    ->todo ADD, DELETE, MARK COMPLETE, DISPLAY functions
    
    ->register and login for user accounts
    
    ->login using github, google
    
    Not Yet Done
    
    ->login using facebook(realize that oauth for facebook must use https, so i tried using ngrok but still not working because Flask is still using http)
                            (If want to change Flask to https need to download OpenSSL, set environment, and many things need to do)

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,17 @@ db = SQLAlchemy(app)
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 
-
+google_bp = make_google_blueprint(
+    client_id="261395564675-6ljhvh2mk0r0s19lu9fgj0tjs8vvrjc8.apps.googleusercontent.com",
+    client_secret="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",
+    redirect_to="google_login",
+    scope=["openid", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"]
+)
+facebook_bp = make_facebook_blueprint(
+    client_id="539202525925799",
+    client_secret="73b4ea4b753d1f553bc681fc0e058662",
+    redirect_to="facebook_login"
+)
 github_bp = make_github_blueprint(
     client_id="Ov23linshJXIEsSRwcwU",
     client_secret="1fc23f0e1d2a3a7f56494f399dd70dc9ec9904da",
@@ -22,7 +32,8 @@ github_bp = make_github_blueprint(
     scope="read:user,user:email"
 )
 
-
+app.register_blueprint(google_bp, url_prefix="/google_login")
+app.register_blueprint(facebook_bp, url_prefix="/facebook_login")
 app.register_blueprint(github_bp, url_prefix="/github_login")
 
 
