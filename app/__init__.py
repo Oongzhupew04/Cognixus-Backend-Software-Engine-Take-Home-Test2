33ee5c1b85bb53630de1c259b0d2409363daa53b
Oongzhupew04 <vincentoong12345@gmail.com>
2025-05-13 02:26:58 +0800

    removed Credentials

--- app/__init__.py
+++ app/__init__.py
@@ -15,19 +15,19 @@ os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 
 google_bp = make_google_blueprint(
-    client_id="261395564675-6ljhvh2mk0r0s19lu9fgj0tjs8vvrjc8.apps.googleusercontent.com",
-    client_secret="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",
+    client_id="",
+    client_secret="",
     redirect_to="google_login",
     scope=["openid", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"]
 )
 facebook_bp = make_facebook_blueprint(
-    client_id="539202525925799",
-    client_secret="73b4ea4b753d1f553bc681fc0e058662",
+    client_id="",
+    client_secret="",
     redirect_to="facebook_login"
 )
 github_bp = make_github_blueprint(
-    client_id="Ov23linshJXIEsSRwcwU",
-    client_secret="1fc23f0e1d2a3a7f56494f399dd70dc9ec9904da",
+    client_id="",
+    client_secret="",
     redirect_to="github_login",
     scope="read:user,user:email"
 )
